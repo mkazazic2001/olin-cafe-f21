f8f7796e765709dcb054e583de2122cffecdc101
Melissa Kazazic <mkazazic01@gmail.com>
2021-09-30 17:32:50 -0400

    Change c_in to wire

--- 01_game_of_life/three_bit_adder.sv
+++ 01_game_of_life/three_bit_adder.sv
@@ -4,7 +4,7 @@ module three_bit_adder(a,b,y,c);
 input wire [2:0] a,b;
 output logic [2:0] y;
 output logic c;
-logic c_in;
+wire c_in;
 
 two_bit_adder two(a[1:0], b[1:0], y[1:0], c_in);
 full_adder full(a[2], b[2], c_in, y[2],c);
--- 01_game_of_life/two_bit_adder.sv
+++ 01_game_of_life/two_bit_adder.sv
@@ -4,7 +4,7 @@ module two_bit_adder(a,b,y,c);
 input wire [1:0] a,b;
 output logic [1:0] y;
 output logic c;
-logic c_in;
+wire c_in;
 
 half_adder half(a[0], b[0], y[0], c_in);
 full_adder full(a[1], b[1], c_in, y[1],c);
