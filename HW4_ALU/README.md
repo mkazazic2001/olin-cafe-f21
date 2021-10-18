e3509b3beb54ce10629cbe04039155660047aced
Melissa Kazazic <mkazazic01@gmail.com>
2021-10-18 19:55:32 -0400

    Complete mux32 test workbench

--- HW4_ALU/README.md
+++ HW4_ALU/README.md
@@ -1,6 +1,8 @@
-# HW4 Problem 2) Combination Logic Review and HDL Practice: ALU
+# HW4.2) Combination Logic Review and HDL Practice: ALU
 ## The Problem Solution
 
 ## Testing `mux32`
 
 ## How to Run the Tests
+* To run the **Adder** Tests, you call `make test_add32` in the terminal. This script runs tests on the helper `full_adder.sv`, and the final `add32.sv`.
+* To run the **Mux** Tests, you call `make test_mux32` in the terminal. This script runs tests on the helpers `mux2.sv`, `mux4.sv`, `mux8.sv`, and `mux16.sv`, and the final `mux32.sv`.
---
+++ HW4_ALU/test_mux32.sv
@@ -0,0 +1,53 @@
+`timescale 1ns / 1ps
+`default_nettype none
+
+`define SIMULATION
+
+module test_adders;
+  logic [1023:0] D;
+  logic [4:0] S;
+  logic [31:0] Y_2_1;
+  logic [31:0] Y_32_1;
+  logic [31:0] value_split;
+  
+
+  mux2 UUT2_1_MUX (D[63:0], S[0], Y_2_1[31:0]);
+  mux32 UUT32_1_MUX (D[1023:0], S[4:0], Y_32_1[31:0]);
+
+  initial begin
+    // Collect waveforms
+    $dumpfile("muxes.vcd");
+    $dumpvars(0, UUT2_1_MUX);
+    $dumpvars(0, UUT32_1_MUX);
+
+    $display("\n\n32-Bit-Wide 2:1 Mux: \n");
+    
+    D = {32{1'b1}}<<32;
+    #1 for (int i = 0; i < 2; i = i + 1) begin
+      value_split = D>>i*32;
+      #1 if (value_split != Y_2_1) begin
+          $error("Got %32b, Expected %32b", Y_2_1, value_split);
+      end
+    end
+    $display("\n Tested. \n");
+    $display("-------------------------------------------------------------------");
+
+    $display("\n32-Bit-Wide 32:1 Mux:\n");
+    for (int i = 0; i < 32; i = i + 1) begin
+        D = D | $random%4294967295<<i*32;
+    end
+
+    #1 $display("Randomly Generated 1024-bit string: \n\n%1024b", D);
+    for (int i = 0; i < 32; i = i + 1) begin
+      value_split = D>>i*32;
+      #1 if (value_split != Y_32_1) begin
+          $error("Got %32b, Expected %32b", Y_32_1, value_split);
+      end
+    end
+    $display("\n Tested. \n");
+    
+    
+    $finish;
+    end
+
+endmodule
