9dcd5aef4afd8d015b31049242af73f76924a610
Melissa Kazazic <mkazazic01@gmail.com>
2021-10-18 15:24:46 -0400

    Fix 8-input mux

--- HW4_ALU/mux8.sv
+++ HW4_ALU/mux8.sv
@@ -3,7 +3,7 @@ module mux8(D,S,Y);
 input wire [255:0] D;
 input wire [2:0] S;
 output logic [31:0] Y;
-logic [63:0] Y_1, Y_2;
+logic [31:0] Y_1, Y_2;
 
 mux4 pair_1_mux (D[255:128], S[2:1], Y_1);
 mux4 pair_2_mux (D[127:0], S[2:1], Y_2);
