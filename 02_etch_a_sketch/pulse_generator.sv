3c4d3fc180d37d1bb9625451475b322fefaaede5
Melissa Kazazic <mkazazic01@gmail.com>
2021-10-21 17:35:37 -0400

    Complete pulse generator

--- 02_etch_a_sketch/pulse_generator.sv
+++ 02_etch_a_sketch/pulse_generator.sv
@@ -12,4 +12,21 @@ output logic out;
 logic [N-1:0] counter;
 logic counter_comparator;
 
+always_ff @(posedge clk) begin : counter_logic
+  if (rst | counter_comparator) begin
+    counter <= 0;
+  end else if (ena) begin
+    if(counter_comparator) begin
+      counter <=0;
+    end
+    else begin
+      counter <= counter + 1;
+    end
+  end
+end
+
+always_comb counter_comparator = counter >= (ticks -1);
+
+always_comb out = counter_comparator & ena;
+
 endmodule
